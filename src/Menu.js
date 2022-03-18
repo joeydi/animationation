f3389102b918cd239fba711f37a27fe86dc28964
Joe di Stefano <joeydi@okaypl.us>
2022-03-18 15:08:26 -0400

    cleanup

--- src/Menu.js
+++ src/Menu.js
@@ -3,7 +3,7 @@ import { animated } from "react-spring";
 import "./Menu.scss";
 
 const Menu = ({ springProps }) => {
-    const videos = ["/videos/reelEdit_animationation_v2.mp4", "/videos/oldSchoolCartoons_edit.mp4", "/videos/01_frameByFrame_ball.mp4", "/videos/02_easing_linear.mp4", "/videos/03_easing_inOut.mp4", "/videos/04_easing_in.mp4", "/videos/05_easing_out.mp4", "/videos/animationPrinciples.mp4"];
+    const videos = ["https://simple-creature-website-assets.s3.amazonaws.com/simplecreature/animationation/reelEdit_animationation_v2.mp4", "https://simple-creature-website-assets.s3.amazonaws.com/simplecreature/animationation/oldSchoolCartoons_edit.mp4", "https://simple-creature-website-assets.s3.amazonaws.com/simplecreature/animationation/01_frameByFrame_ball.mp4", "https://simple-creature-website-assets.s3.amazonaws.com/simplecreature/animationation/02_easing_linear.mp4", "https://simple-creature-website-assets.s3.amazonaws.com/simplecreature/animationation/03_easing_inOut.mp4", "https://simple-creature-website-assets.s3.amazonaws.com/simplecreature/animationation/04_easing_in.mp4", "https://simple-creature-website-assets.s3.amazonaws.com/simplecreature/animationation/05_easing_out.mp4", "https://simple-creature-website-assets.s3.amazonaws.com/simplecreature/animationation/animationPrinciples.mp4"];
 
     return (
         <animated.div className="menu" style={springProps}>
